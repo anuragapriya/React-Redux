4a728a01f09ef9487c2b5e1bd0e65cdca0168b77
chilukashivakumar <c.sivakumarnova@gmail.com>
2024-12-05 18:53:18 +0530

    Zoom Functionality

--- src/container/layout/LoginLayout.js
+++ src/container/layout/LoginLayout.js
@@ -11,19 +11,53 @@ const LoginLayout = () => {
     if (auth) {
         return <Navigate to="/home" />;
     }
+    const setzoom125 =()=>{
+        const element =document.getElementById('zoomadd')
+        element.classList.remove('zoom100')
+        element.classList.remove('zoom150')
+       element.classList.add('zoom125')
+      
+
+    }
+    const setzoom100 =()=>{
+        const element =document.getElementById('zoomadd')
+        element.classList.remove('zoom125')
+        element.classList.remove('zoom150')
+        element.classList.add('zoom100')
+        
+
+    }
+    const setzoom150 =()=>{
+        const element =document.getElementById('zoomadd')
+        element.classList.remove('zoom100')
+        element.classList.remove('zoom125')
+        element.classList.add('zoom150')
+    }
 
     return (
         <div className=" p-0">
             <div className="row m-0">
-            <button className="add" >
-            125</button>
-                <div className="col-sm-12 login-container p-0">
+            <div className="zoom" id="zoomadd">
+                    <div className=" login-container" >
+
+                        <div className="App">
+                            <button className="add" onClick={setzoom125}>
+                                125</button>
+                            <button className="remove" onClick={setzoom100}>
+                                100</button>
+                            <button className="toggle" onClick={setzoom150}> 150
+                            </button>
+
+
+                        </div>
+                
                     <Routes>
                         <Route element={<MainLayout />}>
                             <Route index element={<Login />} />
                             <Route path="register" element={<Register />} /> 
                         </Route>
                     </Routes>
+                    </div>
                 </div>
             </div>
         </div>
