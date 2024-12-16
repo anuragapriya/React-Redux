86a8484aa6bdff2aa0c909dd36ccebfaa3b015cc
Anuragapriya Muthiri <anurag8414@gmail.com>
2024-12-16 18:28:11 +0530

    revert back to old

--- src/container/layout/LoginLayout.js
+++ src/container/layout/LoginLayout.js
@@ -5,24 +5,21 @@ import { Register } from 'container/registration';
 import { MainLayout, RegisterMainLayout } from 'container/layout';
 
 const LoginLayout = ({ isAuthenticated }) => {
+  // Redirect to home if already logged in
+  if (isAuthenticated) {
+    return <Navigate to="/home" />;
+  }
 
-    // redirect to home if already logged in
-    if (isAuthenticated) {
-        return <Navigate to="/home" />;
-    }
-
-    return (
-        <div className=" login-container" >
-            <Routes>
-                <Route element={<MainLayout />}>
-                    <Route index element={<Login />} />
-                </Route>
-                <Route element={<RegisterMainLayout />}>
-                    <Route path="register" element={<Register />} />
-                </Route>
-            </Routes>
-        </div>
-    );
-}
+  return (
+    <div className="login-container">
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default LoginLayout;
