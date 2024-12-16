8bbac91ee83260eac40140ef7d20bbfe76c47bbe
Raghavendracn <raghavendracn66@gmail.com>
2024-12-16 18:37:45 +0530

    Upload functionality is ready in Supplierdiversity

--- src/_components/UploadFiles.js
+++ src/_components/UploadFiles.js
@@ -35,7 +35,7 @@ export default function UploadFiles() {
                 <Grid container spacing={3}>
                     <Grid item xs={12} sm={12} md={12}>
                         <Typography className="Personal-Informationsheading" >
-                            <Typography component="h2">Upload your Documents </Typography>
+                            {/* <Typography component="h2">Upload your Documents </Typography> */}
                         </Typography>
                         <Button
                             component="label"
--- src/container/suplierDiversity/ManageProfileSD.js
+++ src/container/suplierDiversity/ManageProfileSD.js
@@ -1,6 +1,118 @@
-const ManageProfileSD =()=>
-    {
-        return <></>;
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useDispatch, useSelector } from 'react-redux';
+import { Typography, Button } from '@mui/material';
+import { alertActions, userActions } from '_store';
+import { useNavigate, useParams } from 'react-router-dom';
+import { additionalDetailsValidationSchema, companyValidationSchema } from "_utils/validationSchema";
+import { CompanyDetails } from "container/user";
+import AdditionalDetails from "container/user/ProfileDetails/AdditionalDetails";
+import Grid from "@material-ui/core/Grid";
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { UploadFiles } from '_components';
+
+
+const ManageProfileSD = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const user = useSelector(x => x.users?.item);
+
+    const combinedSchema = additionalDetailsValidationSchema.concat(companyValidationSchema);
+    const { register, handleSubmit, control, reset, formState: { errors, isSubmitting }, watch, trigger } = useForm({
+        resolver: yupResolver(combinedSchema)
+    });
+
+    useEffect(() => {
+        dispatch(userActions.clear());
+        if (id) {
+
+            dispatch(userActions.getById(id)).unwrap().then(user => reset(user));
+        } else {
+
+            reset(user); // Reset form state when adding a new user
+        }
+    }, [id, dispatch, reset]);
+
+    const onSubmit = async (data) => {
+        dispatch(alertActions.clear());
+        try {
+            // await dispatch(registrationActions.register(data)).unwrap();
+            // navigate('/');
+            // dispatch(alertActions.success({
+            //     showAfterRedirect: true,
+            //     message: verifyEmailLabels.message1,
+            //     message2: verifyEmailLabels.message2,
+            //     header: verifyEmailLabels.header
+            // }));
+
+        } catch (error) {
+            dispatch(alertActions.error({ message: error.message, header: "Registration Failed" }));
+        }
     };
-    
-    export default ManageProfileSD;
+    const docsType = ["Certification Document", "Capability Statement", "Additional Documents"];
+    const getSelectedDoctype= (value)=>{
+        console.log(value);
+    }
+    return <>
+        <Typography component="div" className="MapCenterAccecss">
+            <Typography component="div" className="MapCenterAccecssheading">
+                <Typography component="h1" variant="h5">Supplier Diversity</Typography>
+            </Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={12} md={12} className="Personal-Information-container">
+                        <Grid container spacing={3}>
+                            <Grid item xs={12} sm={5} md={4} className="Personal-Information">
+                                <Typography component="div" className="mapcontainer">
+                                    <Typography component="div" className="Personal-Informationsheading">
+                                        <Typography component="h2" variant="h5">Personal Information</Typography>
+                                    </Typography>
+                                    {/* <AdditionalDetails register={register} errors={errors} control={control} trigger={trigger} /> */}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={5} md={4} className="Personal-Information">
+
+                                {/* <Typography component="div" className="mapcontainer">
+                                    <Typography component="div" className="Personal-Informationsheading">
+                                        <Typography component="h2" variant="h5">Company Point of Contact</Typography>
+                                    </Typography>
+                                    <CompanyDetails register={register} errors={errors} control={control} trigger={trigger} />
+                                </Typography> */}
+                            </Grid>
+                            <Grid item xs={12} sm={5} md={4} >
+                                <Typography component="div" className="Personal-Informationsheading">
+                                    <Typography component="h2" variant="h5" >Documents uplaod</Typography>
+                                    <Autocomplete
+                                        disablePortal
+                                        options={docsType}
+                                        sx={{ width: 300 }}
+                                        getSelectedDoctype={(option)=> option}
+                                        renderInput={(params) => <TextField {...params} label="Document Type" />}
+                                    />
+                                    <UploadFiles />
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} className="Personal-Information">
+                        <Button variant="contained" color="primary" disabled={isSubmitting}>
+                            Cancel
+                        </Button>
+                        <Button type="submit" variant="contained" color="primary" disabled={isSubmitting}>
+                            Complete Registration
+                        </Button>
+                    </Grid>
+
+                </Grid>
+
+
+            </form>
+        </Typography>
+
+    </>;
+};
+
+export default ManageProfileSD;
