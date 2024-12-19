b195bd6e9e74c64f681d09259ef4b90a96a46ccd
Raghavendracn <raghavendracn66@gmail.com>
2024-12-19 19:47:01 +0530

    Autocomplete multiselect is working

--- src/container/user/ProfileDetails/SupplierDetails.js
+++ src/container/user/ProfileDetails/SupplierDetails.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Grid from "@material-ui/core/Grid";
 import { CustomFormControl } from '_components';
-import { AutocompleteInput, MobileNumberInput } from '_components'
-import { supplierDocumentTypeData } from '_utils/constant';
+import { AutocompleteInput, MobileNumberInput, MultiSelectAutocomplete } from '_components'
+import { supplierClassificationData , supplierBusinessData} from '_utils/constant';
 const SupplierDetails = ({ register, errors, control, trigger }) => {
     const [inputColors, setInputColors] = useState({});
+    const [selectedOptions, setSelectedOptions] = useState([]);
+
     const handleBlur = (e) => {
         const fieldName = e.target.name;
         const fieldError = errors[fieldName];
@@ -16,14 +18,24 @@ const SupplierDetails = ({ register, errors, control, trigger }) => {
 
         trigger(fieldName); // Trigger validation for the field
     };
+    const handleSelectionChange = (newValue) => {
+        console.log(newValue);
+        setSelectedOptions(newValue);
+        console.log(selectedOptions);
+      };
 
-    const documentData = supplierDocumentTypeData.map(x => ({
+
+    const supplierClassificationDropdownData = supplierClassificationData.map(x => ({
         label: x.DocumentDescription,
         value: x.DocumentId
-      }));
+    }));
+    const supplierBusinessDropdownData = supplierBusinessData.map(x => ({
+        label: x.DocumentDescription,
+        value: x.DocumentId
+    }));
     return <>
         <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
                 <CustomFormControl
                     id="companyName"
                     label="Company Name"
@@ -34,7 +46,7 @@ const SupplierDetails = ({ register, errors, control, trigger }) => {
                     inputColors={inputColors}
                 />
             </Grid>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
                 <CustomFormControl
                     id="companyWebsite"
                     label="Company Website"
@@ -45,144 +57,146 @@ const SupplierDetails = ({ register, errors, control, trigger }) => {
                     inputColors={inputColors}
                 />
             </Grid>
-       
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
-            <AutocompleteInput
-                id="businessCatagory"
-                name="businessCatagory"
-                label="Business Category"
-                control={control}
-                options={documentData}
-                error={!!errors.businessCatagory}
-                helperText={errors.businessCatagory?.message}
-                handleBlur={handleBlur}
-                // onFocus={handleOtherFocus}
-                inputColor={inputColors['selectPortal']}
-            />
+
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
+                <AutocompleteInput
+                    id="businessCatagory"
+                    name="businessCatagory"
+                    label="Business Category"
+                    control={control}
+                    options={supplierBusinessDropdownData}
+                    error={!!errors.businessCatagory}
+                    helperText={errors.businessCatagory?.message}
+                    handleBlur={handleBlur}
+                    // onFocus={handleOtherFocus}
+                    inputColor={inputColors['selectPortal']}
+                />
             </Grid>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
-            <AutocompleteInput
-                id="classification"
-                name="classification"
-                label="Classification"
-                control={control}
-                options={documentData}
-                error={!!errors.classification}
-                helperText={errors.classification?.message}
-                handleBlur={handleBlur}
-            />
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
+                <MultiSelectAutocomplete
+                    id="classification"
+                    name="classification"
+                    label="Classification"
+                    value={selectedOptions}
+                    control={control}
+                    options={supplierClassificationDropdownData}
+                    onChange={handleSelectionChange}
+                    error={!!errors.classification}
+                    helperText={errors.classification?.message}
+                    handleBlur={handleBlur}
+                    // onFocus={handleClassificationFocus}
+                    inputColor={inputColors['selectPortal']}
+                />
             </Grid>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
-            <CustomFormControl
-                id="services"
-                label="Services / Products Provided"
-                type="text"
-                register={register}
-                errors={errors}
-                handleBlur={handleBlur}
-                inputColors={inputColors}
-            />
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
+                <CustomFormControl
+                    id="services"
+                    label="Services / Products Provided"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
-            <CustomFormControl
-                id="expirydate"
-                label="Expiry Date of the Certificate"
-                type="text"
-                register={register}
-                errors={errors}
-                handleBlur={handleBlur}
-                inputColors={inputColors}
-            />
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
+                <CustomFormControl
+                    id="expirydate"
+                    label="Expiry Date of the Certificate"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
-            <CustomFormControl
-                id="contactperson"
-                label="Contact Person"
-                type="text"
-                register={register}
-                errors={errors}
-                handleBlur={handleBlur}
-                inputColors={inputColors}
-            />
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
+                <CustomFormControl
+                    id="contactperson"
+                    label="Contact Person"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
-            <CustomFormControl
-                id="title"
-                label="Title"
-                type="text"
-                register={register}
-                errors={errors}
-                handleBlur={handleBlur}
-                inputColors={inputColors}
-            />
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
+                <CustomFormControl
+                    id="title"
+                    label="Title"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
-            <CustomFormControl
-                id="email"
-                label="Email"
-                type="text"
-                register={register}
-                errors={errors}
-                handleBlur={handleBlur}
-                inputColors={inputColors}
-            />
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
+                <CustomFormControl
+                    id="email"
+                    label="Email"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
-            <Grid item xs={12} sm={6}  md={6}  className='supplierDetailes'>
-            <MobileNumberInput
-                control={control}
-                name="mobileNumber"
-                label="Phone Number"
-                rules={{ required: 'Phone Number is required' }}
-                errors={errors}
-                handleBlur={handleBlur}
-                inputColors={inputColors}
-            />
-             </Grid>
-             <Grid item xs={12} sm={12} md={12} className='supplierDetailes'>
-            <CustomFormControl
-                id="address"
-                label="Address"
-                type="text"
-                register={register}
-                errors={errors}
-                handleBlur={handleBlur}
-                inputColors={inputColors}
-            />
+            <Grid item xs={12} sm={6} md={6} className='supplierDetailes'>
+                <MobileNumberInput
+                    control={control}
+                    name="mobileNumber"
+                    label="Phone Number"
+                    rules={{ required: 'Phone Number is required' }}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
-            <Grid item xs={12} sm={6} md={4} className='supplierDetailes'>
-            <AutocompleteInput
-                id="state"
-                name="state"
-                label="State"
-                control={control}
-                options={documentData}
-                error={!!errors.state}
-                helperText={errors.state?.message}
-                handleBlur={handleBlur}
-            />
+            <Grid item xs={12} sm={12} md={12} className='supplierDetailes'>
+                <CustomFormControl
+                    id="address"
+                    label="Address"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
             <Grid item xs={12} sm={6} md={4} className='supplierDetailes'>
-            <AutocompleteInput
-                id="country"
-                name="country"
-                label="Country"
-                control={control}
-                options={documentData}
-                error={!!errors.country}
-                helperText={errors.country?.message}
-                handleBlur={handleBlur}
-            />
+                <CustomFormControl
+                    id="city"
+                    label="City"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
             <Grid item xs={12} sm={6} md={4} className='supplierDetailes'>
-            <CustomFormControl
-                id="zipcode"
-                label="ZipCode"
-                type="text"
-                register={register}
-                errors={errors}
-                handleBlur={handleBlur}
-                inputColors={inputColors}
-            />
+                <CustomFormControl
+                    id="state"
+                    label="State"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} className='supplierDetailes'>
+                <CustomFormControl
+                    id="zipcode"
+                    label="ZipCode"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    handleBlur={handleBlur}
+                    inputColors={inputColors}
+                />
             </Grid>
         </Grid>
     </>
